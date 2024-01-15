89e770bde3fd22222548776575b58cb9a5b68b0e
huyanhit <huyanhit@gmail.com>
2024-01-15 18:26:27 +0700

    first commit

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Treatment extends Model
 {
     use HasFactory;
+    
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
